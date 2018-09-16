13cd7bb4ed89745aaea5ebf0769e7e825c449eea
error418 <error418@users.noreply.github.com>
2018-09-16 20:42:29 +0200

    fix:  reference error. random quote was not working (whoops)

--- routes/quote.js
+++ routes/quote.js
@@ -6,14 +6,14 @@ router.get('/:quoteSource', function(req, res) {
   var pick = req.query.pick
   var quoteSource = req.params.quoteSource
 
-  var quotes = req.app.locals[quoteSource]
-  if(quotes) {
+  var collection = req.app.locals[quoteSource]
+  if(collection) {
     if(pick === "random") {
       res.send({
-        q: quotes[Math.floor(Math.random() * quotes.length)]
+        q: collection.quotes[Math.floor(Math.random() * collection.quotes.length)]
       })
     } else {
-      res.send(quotes)
+      res.send(collection)
     }
   } else {
     res.send(404)
